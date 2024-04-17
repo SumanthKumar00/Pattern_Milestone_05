cc0190bfe52126f8741f79baf9480d3f69003f17
Sumanth Kumar Sivadi <S567094@nwmissouri.edu>
2024-04-17 18:40:11 -0500

    push the DogsManagementSystem code into repository

---
+++ target/src/main/java/com/sivadiClasses/DMS/repository/TrainerRepository.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.sivadiClasses.DMS.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.sivadiClasses.DMS.Models.Trainer;
+
+/**
+ * 
+ */
+public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
+
+}
